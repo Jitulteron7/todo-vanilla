0c6a1e0d36006f84a1a6a76354da094619df9bbb
Jitulteron7 <jitulteron9@gmail.com>
2023-08-15 02:20:10 +0530

    todo-list optimization of todo-app

--- todo.js
+++ todo.js
@@ -1,9 +1,11 @@
 //get
+const todoContainer = document.getElementById("todolist-container");
+
 const showTodos = (datas) => {
-  const todoContainer = document.getElementById("todolist-container");
   todoContainer.innerHTML = "";
   for (let data of datas) {
     const div = document.createElement("div");
+    div.id = data.id;
     const h4 = document.createElement("h4");
     h4.innerText = data.title;
     const span1 = document.createElement("span");
@@ -17,7 +19,7 @@ const showTodos = (datas) => {
       checkTodo(data.id, !data.check);
     };
     btn.onclick = function () {
-      deleteTodo(data.id);
+      deleteTodo(data.id, todoContainer.children);
     };
     btn.innerText = "Delete";
     span2.append(checkbox);
@@ -37,6 +39,8 @@ const getTodos = () => {
   })
     .then((res) => res.json())
     .then((res) => {
+      console.log(res);
+      res.sort((a, b) => -a.createdAt + b.createdAt);
       showTodos(res);
     })
     .catch((err) => {
@@ -64,9 +68,32 @@ const postTodo = () => {
   })
     .then((res) => res.json())
     .then((res) => {
-      document.getElementById("title").value = "";
-      document.getElementById("desc").value = "";
-      getTodos();
+      const div = document.createElement("div");
+      div.id = res.id;
+      const h4 = document.createElement("h4");
+      h4.innerText = res.title;
+      const span1 = document.createElement("span");
+      span1.innerText = res.desc;
+      const span2 = document.createElement("span");
+      const btn = document.createElement("button");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = res.check;
+      checkbox.onclick = function () {
+        checkTodo(res.id, !res.check);
+      };
+      btn.onclick = function () {
+        deleteTodo(res.id, todoContainer.children);
+      };
+      btn.innerText = "Delete";
+      span2.append(checkbox);
+      span2.append(btn);
+      div.append(h4);
+      div.append(span1);
+      div.append(span2);
+      todoContainer.append(div);
+
+      //   getTodos();
     })
     .catch((err) => {
       console.log(err);
@@ -74,14 +101,16 @@ const postTodo = () => {
 };
 
 //delete
-const deleteTodo = (id) => {
+const deleteTodo = (id, children) => {
   fetch(`https://64d74eef2a017531bc132085.mockapi.io/todos/${id}`, {
     method: "delete",
   })
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
-      getTodos();
+      children = Array.from(children);
+      idx = children.map((child) => child.id).indexOf(id);
+      todoContainer.removeChild(children[idx]);
+      console.log(idx);
     })
     .catch((err) => {
       console.log(err);
